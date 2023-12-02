c16b1e28cbd3afd09088744e531b4e95cca1d944
xnm <artem.shv@proton.me>
2023-12-02 19:26:03 +0200

    added devShell for Nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A devShell for Rust Development";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+            pkg-config
+            openssl
+          ];
+        };
+      }
+    );
+}
